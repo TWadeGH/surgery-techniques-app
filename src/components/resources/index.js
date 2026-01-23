f3eb176f4c46aad8ec962fd9a17ac57eac80bec0
TWadeGH <thmoderntx@gmail.com>
2026-01-23 17:43:25 -0600

    Add index.js for resource components exports
    Created an index file to export all resource-related components for easier imports.

---
+++ src/components/resources/index.js
@@ -0,0 +1,12 @@
+/**
+ * Resource Components Index
+ * Export all resource-related components from a single location
+ * 
+ * Usage:
+ * import { ResourceCard, ResourceList, ResourceForm } from './components/resources';
+ */
+
+export { default as ResourceCard } from './ResourceCard';
+export { default as ResourceList } from './ResourceList';
+export { default as ResourceFilters } from './ResourceFilters';
+export { default as ResourceForm } from './ResourceForm';
